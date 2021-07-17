9cbfb1e37c6958d5f6914904fed5dc4d8ab95eff
PhuleArti <77423884+PhuleArti@users.noreply.github.com>
2021-07-17 17:38:39 +0530

    Add files via upload

---
+++ Pattern_Printing_In_Cpp/11.1.cpp
@@ -0,0 +1,45 @@
+/*
+problem statement:
+Accept number from user and print below pattern.
+input:5
+output: A  B  C   D   E
+
+*/
+using namespace std;
+
+#include<iostream>
+
+class Display
+{
+      public:
+                int no1;
+                
+      public:
+      void DisplayPattern(int no1)
+      {
+            int i=0;
+            char ch='A'; 
+            
+            if(no1<0)
+            {
+                 no1=-no1;
+            }
+            
+            for(i=1;i<=no1;i++)        
+            {
+                cout<<ch++;
+            }  
+      }           
+};
+int main()
+{
+       int  value1=0;
+               
+       cout<<"enter number"<<"\n";
+       cin>>value1; 
+            
+       Display obj1; 
+       obj1.DisplayPattern(value1);
+}
+
+
